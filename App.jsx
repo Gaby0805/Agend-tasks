2ffdb63e7e5f65fc4c6c4d2759dc8cd710480af0
Gaby Amorim <gabbiebrabo@gmail.com>
2025-04-28 18:24:06 -0400

    pagina de adicionar tarefa

--- App.jsx
+++ App.jsx
@@ -5,12 +5,14 @@ import { View, Text } from 'react-native';
 import { createStaticNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/home';
+import Addpage from './src/addpage';
 
 
 const RootStack = createNativeStackNavigator({
   screenOptions: {headerShown: false},
   screens: {
     Home: Home,
+    ADD: Addpage
   },
 });
 
--- src/addpage.jsx
+++ src/addpage.jsx
@@ -5,42 +5,27 @@ import { Icon } from 'react-native-elements'
 import Task from './components/tarefas';
 import tarefasJson from '../database/bd.json';
 import { ScrollView } from 'react-native-web';
-import { useNavigation } from '@react-navigation/native';
 
 
-const App = () => {
-  const navigation = useNavigation();
+export default function Addpage() {
+
     const tarefas = tarefasJson.Tarefas;
     
     return (
     <View className='flex-1 '>
-    <Header param={'Abril/2025'} />
+    <Header param={'Adicionar tarefa'} />
 
 
     
         <View className='Main  items-center flex-1'>
 
-            <View className='w-full h-5 flex justify-center items-center mt-8'>
-                <Text className='text-3xl'>Tarefas</Text>
-            </View>
-        <ScrollView className='w-11/12'>
-  {tarefas.map((items) => (
-    <Task
-      key={items.id}
-      nome={items.nome_tarefa}
-      Categoria={items.categoria}
-      Data={items.data_final}
-      Status={items.status}
-    />
-  ))}
-</ScrollView>
-        
+
 
         </View>
 
     <View className=' w-full h-fit  flex justify-end items-end'>
     <TouchableOpacity className='w-10 h-10 rounded-full flex  bg-blue-700 justify-center items-center text-2xl m-4 pointer-events-auto'
-    onPress={()=> {navigation.navigate('ADD')}}
+    onPress={()=> {alert('return button ')}}
     >
     <Icon
         name='add'
@@ -52,4 +37,3 @@ const App = () => {
   );
 };
 
-export default App;
